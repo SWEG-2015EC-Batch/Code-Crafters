80fad28030abf21cf6410186752ed9f71f89b76a
YaredAbebe <149156568+YaredAbebe01@users.noreply.github.com>
2023-11-15 11:14:19 +0300

    Update ETS1378_15_YARED_ABEBE

---
+++ ETS1378_15_YARED_ABEBE/Activity2.2 question number 5 code inside.cpp
@@ -0,0 +1,25 @@
+5,
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+  string Name;
+  double weekly_working_hour, bounus_rate_per_hour, base_salary, gross_salary, net_salary, bounus_payment ;
+  double const tax_rate = 0.15, pension_rate = 0.05;
+  cout << "Employer name: ";
+  cin >> Name;
+  cout << "Weekly working hour= ";
+    cin >> weekly_working_hour;
+  cout << "Bounus rate per hour= ";
+  cin >> bounus_rate_per_hour;
+  cout << "Base salary= ";
+  cin >> base_salary;
+  gross_salary =  base_salary + (bounus_rate_per_hour * weekly_working_hour);
+  net_salary = gross_salary* (1- tax_rate - pension_rate);
+  bounus_payment = bounus_rate_per_hour * weekly_working_hour;
+  cout << "Gross salary= " << gross_salary<< " Birr"<<endl;
+  cout << "Net salary= " << net_salary<<" Birr"<<endl;
+  cout << "Bounus payment= " << bounus_payment<<" Birr"<<endl;
+  return 0;
+}
