1d5cd041747cc5d5fbd08d33dcb263ddc5bf53ab
YaredAbebe <149156568+YaredAbebe01@users.noreply.github.com>
2023-11-15 11:10:21 +0300

    Update ETS1378_15_YARED_ABEBE

---
+++ ETS1378_15_YARED_ABEBE/Activity2.2 question number 2 code inside.cpp
@@ -0,0 +1,14 @@
+2,
+#include <iostream>
+using namespace std;
+int main()
+{     double fule_tank_capacity, mile_per_gallon, number_of_mile;
+    cout << "Enter fule tank capacity= "; 
+    cin >> fule_tank_capacity;
+    cout << "Enter mile per gallon= ";
+    cin >> mile_per_gallon;
+    number_of_mile = fule_tank_capacity * mile_per_gallon;
+    cout << "Number of mile car driven without refuleing is: " << number_of_mile;
+    return 0;
+  
+  }
