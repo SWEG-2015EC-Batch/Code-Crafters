d051ee3e863869c93625abb010345c9a3bea490d
YaredAbebe <149156568+YaredAbebe01@users.noreply.github.com>
2023-11-15 11:11:45 +0300

    Update ETS1378_15_YARED_ABEBE

---
+++ ETS1378_15_YARED_ABEBE/Activity2.2 question number 3 code inside.cpp
@@ -0,0 +1,17 @@
+3, 
+#include <iostream>
+#include<ctype.h>
+using namespace std;
+int main()
+{
+  cout << "write a letter: ";
+  char letter,uppercase,lowercase;
+  cin >> letter;
+  lowercase = tolower(letter);
+  uppercase = toupper(letter);
+  if (letter >= 'a' && letter <= 'z')
+  cout<<"Uppercase: "<< uppercase<<endl;
+    else
+  cout << "Lowercase: " << lowercase << endl;
+      return 0;
+}
