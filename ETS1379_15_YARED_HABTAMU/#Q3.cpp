24a90d9c7d7553a6ae97d0302e71b6c3a160d498
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2023-11-17 11:05:43 +0300

    Update ETS1379_15_YARED_HABTAMU

---
+++ ETS1379_15_YARED_HABTAMU/#Q3.cpp
@@ -0,0 +1,23 @@
+
+
+include<iostream>
+
+using namespace std;
+int main()
+
+{
+
+   double capacity_in_gallon, mile_per_gallon,number_of_miles ;         //BMI = Body Mass Index
+
+  cout << "Enter the Capacity in gallon of an automobile fuel tank :";
+    cin >> capacity_in_gallon;
+
+  cout << "Enter the Miles per gallon the automobile can be driven :";
+    cin >> mile_per_gallon;
+
+    number_of_miles = capacity_in_gallon * mile_per_gallon ;
+
+    cout << " Number of miles the automobile can be driven is :"<<number_of_miles<<" miles";
+
+    return 0;
+}
