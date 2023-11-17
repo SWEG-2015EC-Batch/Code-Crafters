d3501e97262e4cf4a04f56905b76579c30855883
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2023-11-17 22:20:26 +0300

    Update #Q2.cpp

--- ETS1379_15_YARED_HABTAMU/#Q2.cpp
+++ ETS1379_15_YARED_HABTAMU/#Q2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 
 {
-double capacity_in_gallon,mile_per_gallon,number_of_miles ;  //BMI = Body Mass Index
+double capacity_in_gallon, mile_per_gallon, number_of_miles ;    //BMI = Body Mass Index
 
 cout << "Enter the Capacity in gallon of an automobile fuel tank :";
     cin >> capacity_in_gallon;
