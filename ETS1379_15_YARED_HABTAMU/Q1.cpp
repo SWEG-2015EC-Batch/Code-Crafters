6f1cf6c2b1b6004cd718247117ddba3aa0562752
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2023-11-17 10:57:57 +0300

    Update Q1.cpp

--- ETS1379_15_YARED_HABTAMU/Q1.cpp
+++ ETS1379_15_YARED_HABTAMU/Q1.cpp
@@ -1,1 +1,21 @@
 
+#include<iostream>     
+
+using namespace std;
+
+int main()
+
+{
+  double weight, height,BMI ;      //BMI = Body Mass Index
+
+    cout << "Enter the weight of a person :"<<endl;
+      cin >> weight;
+    cout << "Enter the height of a person :"<<endl;
+      cin >> height;
+
+BMI= (weight)/(height * height);
+ cout<<"The person's Body Mass Index is : "<<BMI<< " Kg/m^2 ";
+
+return 0;
+
+}
