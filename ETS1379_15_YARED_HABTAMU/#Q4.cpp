ed416959351a8ec3c08d133875413d337a36e619
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2023-11-17 19:45:35 +0300

    Update #Q4.cpp

--- ETS1379_15_YARED_HABTAMU/#Q4.cpp
+++ ETS1379_15_YARED_HABTAMU/#Q4.cpp
@@ -1,1 +1,14 @@
+//Q4
+#include<iostream>       
+#include<cmath>  
+{
+    int x,y,power_value;
+    cout<<"Enter x value : ";
+    cin>>x;
+    cout<<"Enter y value : ";
+    cin>>y;
+  power_value = pow(x,y);
+    cout<< power_value;
 
+    return 0;
+}
