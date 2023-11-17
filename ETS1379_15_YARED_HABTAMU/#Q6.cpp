4f4cb5baffb424b80858a0666c844c6df1c0061e
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2023-11-17 21:38:00 +0300

    Update #Q6.cpp

--- ETS1379_15_YARED_HABTAMU/#Q6.cpp
+++ ETS1379_15_YARED_HABTAMU/#Q6.cpp
@@ -1,1 +1,15 @@
 
+//Q6
+#include<iostream>     
+using namespace std;
+int main()
+{
+    double transmisson_speed = 960, file_size = 419430400;
+    int time_in_sec,time_in_day;
+
+    time_in_sec = file_size / transmisson_speed;
+   time_in_day = time_in_sec / 86400;
+    cout<<" It takes "<<time_in_sec<<" Second "<<endl;
+    cout<<" It Approximately takes "<<time_in_day<< " Days";
+
+}
