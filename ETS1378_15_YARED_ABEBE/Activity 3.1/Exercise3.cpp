6cc40f1e9f55cd36d17d93e3e534bb337d7690a9
YaredAbebe <149156568+YaredAbebe01@users.noreply.github.com>
2023-12-04 23:57:01 +0300

    Update Exercise3.cpp

--- ETS1378_15_YARED_ABEBE/Activity 3.1/Exercise3.cpp
+++ ETS1378_15_YARED_ABEBE/Activity 3.1/Exercise3.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 using namespace std;
 int main()
@@ -6,22 +5,31 @@ int main()
 	char text;
 	cout << "Enter the character: ";
 	cin >> text;
-	if (isalpha(text)) {
-		if (isupper(text))
-			cout << "The character is uppercase alphabet";
-		else
-			cout << "The character is lowercase alphabet";
+	if (isalpha(text)) {		
+		if (isupper(text)) {
+			
+			if (text == 'A' || text == 'E'|| text == 'I'|| text == 'O'|| text == 'U')
+				cout << "The character is vowel uppercase alphabet";
+			else
+				cout << "The character is consonant uppercase alphabet";
+		}
+		
+		else if (islower(text)){
+			if (text == 'a' || text == 'e' || text == 'i' || text == 'o' || text == 'u')
+				cout << "The character is vowel lowercase alphabet";
+			else
+				cout << "The character is consonant lowercase alphabet";
+		}
 	}
-
 	else if (isdigit(text)) {
-		cout << "The character is number ";
-
+		if (text % 2 == 0)
+			cout << "The character is even number ";
+		else
+			cout << "The character is odd number ";
 	}
 	else 
 		cout << "The character is special character";
-	
 	return 0;
 	
 }
-
 
