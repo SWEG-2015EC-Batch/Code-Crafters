95f74998da0c9789147bdca7bdf2dd3329d9d142
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2023-12-16 17:52:16 +0300

    Update Code.cpp

--- ETS1379_15_YARED_HABTAMU/Income calculator /Code.cpp
+++ ETS1379_15_YARED_HABTAMU/Income calculator /Code.cpp
@@ -1,50 +1,57 @@
 #include<iostream>
 using namespace std;
-int main(){
-   double grossSalary,incomeTaxRate,pentionRate=0.7,netSalaray,bonusRate,overTimePayment,pention,tax;
-   int workedHours,overWorkedHour;
-   cout<<"enter your salery :  ";
-   cin>>grossSalary;
-   if(grossSalary <=200){
-      incomeTaxRate=0;
-   }  
-   else if((grossSalary >200)&&(grossSalary <=600)){
-    incomeTaxRate=0.1;
-   }
-   else if((grossSalary >600)&&(grossSalary <=1200)){
-    incomeTaxRate=0.15;
-   }
-   else if((grossSalary >1200)&&(grossSalary <= 2000)){
-    incomeTaxRate=0.2;
-   }
-   else if((grossSalary >2000)&&(grossSalary <=3500)){
-    incomeTaxRate=0.25;
-   }
-   else{ 
-    incomeTaxRate=0.3;
-   }
-   cout<<"enter your amount of worked hours : ";
-   cin>>workedHours;
-   if(workedHours > 40){
-       overWorkedHour=workedHours-40;
-    cout<<"enter amount of bonus rate : ";
-    cin>>bonusRate;
-    overTimePayment=overWorkedHour*bonusRate;
-     pention=grossSalary*pentionRate;
-     tax=grossSalary*incomeTaxRate;
-     cout<<tax<<"\n" ;
-     cout<<pention<<endl;
-
-     netSalaray=(grossSalary-tax-pention)+overTimePayment;
-     cout<<"your net salary is : "<< netSalaray; 
-   } 
-
-   else{
-    netSalaray=grossSalary-tax-pention;
-    cout<<"your net salary is : "<<netSalaray; 
-   } 
+int main() {
+    double grossSalary, incomeTaxRate, pensionRate = 0.07, netSalaray, bonusRate, overTimePayment, pension, tax;
+    int workedHours, overWorkedHour;
+    cout << "Enter your salary in Birr :  ";
+    cin >> grossSalary ;
+    if (grossSalary <= 200)
+    {
+        incomeTaxRate = 0;
+    }
+    else if (grossSalary > 200 && grossSalary <= 600) {
+        incomeTaxRate = 0.01;
+    }
+    else if (grossSalary > 600 && grossSalary <= 1200)
+    {
+        incomeTaxRate = 0.15;
+    }
+    else if (grossSalary > 1200  && grossSalary <= 2000 )
+    {
+        incomeTaxRate = 0.02;
+    }
+    else if (grossSalary > 2000 && grossSalary <= 3500)
+    {
+        incomeTaxRate = 0.25;
+    }
+    else
+        incomeTaxRate = 0.03;
 
 
+    pension = grossSalary * pensionRate;
+    tax = grossSalary * incomeTaxRate ;
 
- return 0;   
+
+    cout << "Enter your amount of worked hours : ";
+    cin >> workedHours;
+    cout << "Enter an over-time bonus rate/hour: ";
+    cin >> bonusRate;
+
+    cout << "Total amount of Tax is " <<tax<<" Br" <<endl ;
+    cout <<"Total amount of pension " <<pension<<" Br"<< endl;
+
+    if (workedHours > 40)
+    {
+        overWorkedHour = workedHours - 40;
+        overTimePayment = overWorkedHour * bonusRate;
+        netSalaray = (grossSalary - tax - pension) + overTimePayment;
+        cout << "Your Net salary is : " << netSalaray<<" Br";
+    }
+
+    else {
+        netSalaray = grossSalary - tax - pension;
+        cout << "Your Net salary is : " << netSalaray<<" Br";
+    }
+
+    return 0;
 }
