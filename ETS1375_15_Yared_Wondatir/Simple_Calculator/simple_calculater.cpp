d5a6f09f150367188a346685c805ac38642600c6
Ya-Red7 <149231087+Ya-Red7@users.noreply.github.com>
2023-12-17 23:27:43 +0300

    Add files via upload

---
+++ ETS1375_15_Yared_Wondatir/Simple_Calculator/simple_calculater.cpp
@@ -0,0 +1,49 @@
+//simple calculator that inputs two numbers and the operator and outputs the result
+#include <iostream>
+using namespace std;
+
+int main(){
+    float operand1, operand2, result;
+    char exit;
+    char opera;
+    do {
+        cout<<"Please enter the first number: ";
+        cin>>operand1;
+        cout<<"Please enter the operator: ";
+        cin>>opera;
+        cout<<"Please enter the second number: ";
+        cin>>operand2;
+        switch(opera){
+            case '+':
+            cout<<"THE SUM OF "<<operand1<<" AND "<<operand2<<" IS "<<operand1+operand2<<endl;
+            break;
+            case '-':
+            cout<<"THE DIFFERENCE OF "<<operand1<<" AND "<<operand2<<" IS "<<operand1-operand2<<endl;
+            break;
+            case '*':
+            cout<<"THE PRODUCT OF "<<operand1<<" AND "<<operand2<<" IS "<<operand1*operand2<<endl;
+            break;
+            case '/':
+            if(operand2==0){
+                cout<<"Can\'t divide a number by 0"<<endl;
+            }
+            else{
+            cout<<"THE QUOTIENT OF "<<operand1<<" AND "<<operand2<<" IS "<<operand1/operand2<<endl; }
+            break;
+            case '%':
+            if(operand2==0){
+                cout<<"Can\'t divide a number by 0"<<endl;
+            }
+            else{
+                result = int(operand1)% int(operand2);
+            cout<<"THE MUDOLE OF "<<operand1<<" AND "<<operand2<<" IS "<<result<<endl; }
+            break;
+            default:
+            cout<<"PLEASE TRY AGAIN!"<<endl;
+            break;
+        }
+        cout<<"If you want to continue the operation press any number, If not press 0"<<endl;
+        cin>>exit;
+    }   while(exit!='0');
+    return 0;
+}
