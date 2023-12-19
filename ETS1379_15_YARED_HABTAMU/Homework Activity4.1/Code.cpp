f4cda0ed341a3199d8e9f099c5e77d87f591a3d4
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2023-12-19 22:27:15 +0300

    Update Code.cpp

--- ETS1379_15_YARED_HABTAMU/Homework Activity4.1/Code.cpp
+++ ETS1379_15_YARED_HABTAMU/Homework Activity4.1/Code.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+int main()
+{
+  int list;
+  cout << "For How many persons ? ";
+  cin >> list;
+
+  int nums;
+  double sumn = 0, sqre_sumn=0, sqre_num, sumn_sqre_num=0;
+  cout << "Enter the numbers "<<endl;
+  for (int i = 0;i < list;i++) 
+  {
+    cin >> nums;
+    sumn = sumn + nums;
+    sqre_sumn = pow(sumn,2);
+    sqre_num = pow(nums, 2);
+    sumn_sqre_num = sumn_sqre_num + sqre_num;
+  
+  }
+  double mean, variance,standardvn;
+  
+  variance = (sumn_sqre_num - (sqre_sumn / list)) / list;
+  mean = sumn / list;
+  standardvn = sqrt(variance);
+
+  cout << "Mean is :"<<mean << endl;
+  cout << "Their Standard deviation is  :" << standardvn ;
+
+}
