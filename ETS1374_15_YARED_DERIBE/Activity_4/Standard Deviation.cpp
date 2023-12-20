9153ee55f109ff2d8000cfbee5a8eca33cef8680
Yared Deribe <149392861+yaredbderibe@users.noreply.github.com>
2023-12-20 05:26:07 +0300

    Add files via upload

---
+++ ETS1374_15_YARED_DERIBE/Activity_4/Standard Deviation.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+    int num = 1;
+    int sumOfSquare = 0;
+    int sum = 0;
+    double mean, standardDeviation;
+    int count = 1;
+
+    cout << "Enter 0 when you finish" << endl;
+    cout << "Enter the number [" << count << "]: ";
+
+while (num != 0) {
+        cin >> num;
+        sum += num;
+        sumOfSquare += num * num;
+        cout<<"the [ "<<count-1<<" ] number is  : "<<num<<endl;
+        cout<<"--------------------------------"<<endl;
+        count++;
+       cout<<"the [ "<<count-1<<" ] number is"<<num;
+    }
+
+    mean = sum / (count - 2);
+    standardDeviation = sqrt((sumOfSquare - (2 * mean * sum) + (mean * mean * (count - 2))) / (count - 2));
+
+    cout << "The mean is: " << mean << endl;
+    cout << "The standard deviation is: " << standardDeviation << endl;
+
+    return 0;
+}
