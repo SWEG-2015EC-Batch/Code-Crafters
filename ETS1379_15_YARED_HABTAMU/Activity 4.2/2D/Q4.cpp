ebdad1247cd0f36a27e3edba7819455f3a6144d3
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-01-13 12:45:12 +0300

    Update Q4.cpp

--- ETS1379_15_YARED_HABTAMU/Activity 4.2/2D/Q4.cpp
+++ ETS1379_15_YARED_HABTAMU/Activity 4.2/2D/Q4.cpp
@@ -1,1 +1,36 @@
+using namespace std;
+#include<iostream>
+int main()
+{
 
+  int num[3][4];
+  int SUM = 0;
+  cout << "Enter ARRAY elements \n";
+  for (int i = 0;i < 3;i++)
+  {
+    for (int j = 0;j < 4;j++)
+    {
+      cin >> num[i][j];
+      if (num[i][j] % 2 == 0)
+      {
+        SUM = SUM + num[i][j];
+      }
+    }
+  }
+  cout << " ________________________________ "<<endl;
+
+  for (int i = 0;i < 3;i++)
+  {
+    for (int j = 0;j < 4;j++)
+    {
+      if (num[i][j] % 2 == 0)
+        cout << num[i][j]<<"\t | ";
+      else
+        cout << "* "<<"\t | ";
+    }
+    cout << endl;
+  }
+  cout << " -------------------------------- " << endl;
+  cout << "The sum of even integers is " << SUM;
+  return 0;
+}
