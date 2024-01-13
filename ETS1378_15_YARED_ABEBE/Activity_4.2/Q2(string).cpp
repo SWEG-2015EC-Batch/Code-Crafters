4742d1c61880ed4929c8cff96b2cdde595878343
YaredAbebe <149156568+YaredAbebe01@users.noreply.github.com>
2024-01-13 11:22:52 +0300

    Create Q2(string).cpp

---
+++ ETS1378_15_YARED_ABEBE/Activity_4.2/Q2(string).cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+
+  string word;
+  cout << "Enter your word ";
+  getline(cin,word);
+  string copy_word;
+
+  for (int i = word.length() - 1;i >= 0;i--)
+  {
+      copy_word = copy_word + word[i];
+  }
+  if (word == copy_word)
+  {
+    cout << "The word is palindrom ";
+  }
+  else
+  {
+    cout << "It is not ";
+  }
+  return 0;
+}
