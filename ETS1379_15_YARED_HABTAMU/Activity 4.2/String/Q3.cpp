8f808e97a28cac614322f9a7c8dc8b6320c672b6
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-01-13 12:22:23 +0300

    Update Q3.cpp

--- ETS1379_15_YARED_HABTAMU/Activity 4.2/String/Q3.cpp
+++ ETS1379_15_YARED_HABTAMU/Activity 4.2/String/Q3.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+  string word;
+  cout << "Enter a word :";
+  getline(cin, word);
 
+
+  for (char x:word)
+  {
+    if (isalpha(x))
+      cout << x;
+    else
+      continue;
+  }
+}
