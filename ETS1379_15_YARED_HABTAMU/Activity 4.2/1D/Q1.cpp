670569f1ea136bed8caad3f31d254266dbaf4325
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-01-13 12:17:28 +0300

    Update Q1.cpp

--- ETS1379_15_YARED_HABTAMU/Activity 4.2/1D/Q1.cpp
+++ ETS1379_15_YARED_HABTAMU/Activity 4.2/1D/Q1.cpp
@@ -1,1 +1,15 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+  int number[4][5] = { 16,22,99,4,18,-258,4,101,5,98,105,6,15,2,45,33,88,72,16,3 };
+  cout << "numbers are" << endl;
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 5; j++)
+    {
+      cout << number[i][j] << "\t";
+    }
+    cout << endl;
+  }
+}
