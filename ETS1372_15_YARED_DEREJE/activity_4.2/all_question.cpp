28ec07f78f042ddc28042cbb41e99b377e2cf2d9
yared <yarachesu@gmail.com>
2024-01-13 12:24:23 -0800

    activity 2.1 all code in one

---
+++ ETS1372_15_YARED_DEREJE/activity_4.2/all_question.cpp
@@ -0,0 +1,749 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+#include<cctype>
+using namespace std;
+int main(){
+
+cout<<"***********************************************************************"<<endl;
+
+//signed int arry[4][5]={{16,22,99,4,18},{-258,64,101,5,98},{105,6,15,2,45},{33,88,72,16,3}};
+//
+//
+//for(int i=0 ; i<4 ; i++)
+//{
+//	for(int j=0 ; j<5 ; j++)
+//	{
+//		cout<<arry[i][j]<<" ";
+//	}
+//	cout<<endl;
+//}
+//
+   
+     //second question
+     
+//double temp[7];
+//double sum = 0;
+//for(int i=0 ; i<=7 ; i++)
+//{
+//	cout<<"enter the [ "<<i+1<<" ]  index : ";
+//	cin>>temp[i];
+//	sum += temp[i];
+//	system("CLS");
+//}
+//
+//for(int i=0 ; i<=7 ; i++)
+//{
+//	cout<<"the [ "<<i+1<<" ]  number is : "<<temp[i]<<endl;
+//}
+//
+//
+//cout<<"the sum of the number you entered is : "<<sum;
+//
+
+      //third question
+      
+
+
+
+
+//
+//float first[2][2];
+//for(int i=0 ; i<2 ; i++)
+//{
+//	for(int j=0 ; j<2 ; j++)
+//	{
+//		cout<<"enter [ "<< i+1 <<" ]  row [ "<<j+1<<" ] column ";
+//		cin>>first[i][j];
+//		system("CLS");
+//	}
+//}
+//
+//cout<< "please enter second table data"<<endl;
+//float second[2][2];
+//for(int i=0 ; i<2 ; i++)
+//{
+//	for(int j=0 ; j<2 ; j++)
+//	{
+//		cout<<"enter [ "<< i+1 <<" ]  row [ "<<j+1<<" ] column ";
+//		cin>>second[i][j];
+//		system("CLS");
+//	}
+//}
+//
+//float sum[2][2];
+//for(int i=0 ; i<2 ; i++)
+//{
+//	for(int j=0 ; j<2 ; j++)
+//	{
+//	
+//			sum[i][j] = first[i][j] + second[i][j];
+//	
+//	}
+//}
+//
+//
+//
+//for(int i=0 ; i<2 ; i++)
+//{
+//	for(int j=0 ; j<2 ; j++)
+//	{
+//	    cout<<sum[i][j]<<", ";
+//	}
+//	cout<<endl;
+//}
+
+     
+     //fourth quetion
+     
+//
+//float volts[3][3] = {{11.95,16.32,12.15},{8.22,15.98,26.22},{13.54,6.45,17.59}};
+//
+//for(int i=0 ; i<3 ; i++)
+//{
+//	for(int j=0 ; j<3 ; j++)
+//	{
+//		cout<<volts[i][j]<<" ";
+//	}
+//	cout<<endl;
+//}
+
+
+
+    // fifth question
+
+
+
+//double mark[5];
+//char exellent[10]="Exellent";
+//char good[10]="good";
+//char fair[10]="fair";
+//char poor[10]="poor";
+//char fail[10]="fail";
+//
+//for(int i=0 ; i<=5 ; i++)
+//{
+//	cout<<"enter student [ "<<i+1<<" ] total mark : ";
+//	cin>>mark[i];
+//	system("CLS");
+//}
+//
+//cout<<"     student              mark               status "<<endl;
+//for(int i=0 ; i<5 ; i++)
+//{
+// cout<<"     student  "<<i+1<<"  "<<"          "<<mark[i]<<"          ";
+// if(mark[i]>80 )
+// {
+// 	cout<<exellent<<endl;
+// }
+// else if(mark[i]>60 && mark[i]<80)
+// {
+// 	cout<<good<<endl;
+// }
+// else if(mark[i]>50 && mark[i]<60)
+// {
+// 	cout<<fair<<endl;
+// }
+// else if(mark[i]>40 && mark[i]<50)
+// {
+// 	cout<<poor<<endl;
+// }
+// else if(mark[i]<40)
+// {
+// 	cout<<fail<<endl;
+// }
+//}
+
+  //print the even number
+//
+//int arr[20];
+//
+//for(int i=0 ; i<5 ; i++)
+//{
+//	cout<<"enter number [ "<<i+1<<" ]";
+//	cin>>arr[i];
+//}
+//
+//for(int i=4 ; i>=0 ; i--)
+//{
+//	int temp =arr[i];
+//	if(temp % 2 == 0)
+//	{
+//		cout<<arr[i]<<",  ";
+//	}
+//}
+
+     
+     
+     //    2D array
+     
+    // cout<<"**********************************************1"<<endl;
+//int num1[20];
+//
+//for(int i=0 ; i<5 ; i++)
+//{
+//	cout<<"enter first array number [ "<<i+1<<" ]";
+//	cin>>num1[i];
+//}
+//
+//int num2[20];
+//
+//for(int i=0 ; i<5 ; i++)
+//{
+//	cout<<"enter second arry number [ "<<i+1<<" ]";
+//	cin>>num2[i];
+//}
+//int count=1;
+//cheakagain:
+//	if(count!=4){
+//	
+//	
+//for(int i=4 ; i>=0 ; i--)
+//{
+//	if(num1[i]==num2[i])
+//	{
+//		count++;
+//		 goto cheakagain;
+//		 
+//	}
+//	else{
+//		cout<<"the two array are not similar";
+//		break;
+//	}
+//}
+//}
+//else{
+//	cout<<"the two array you entered is are the same";
+//}
+
+    //cout<<****************************************************2"<<endl;
+//
+//
+//
+//float number[2][2];
+//float total[20];
+// double multiply[20];
+// for(int i=0 ; i<2 ; i++)
+// {
+//    for(int j=0 ; j<2 ; j++)
+//	{
+//		cout<<"enter "<<i<<" row and [ "<<j<<" ] column : ";
+//		cin>>number[i][j];
+//		system("CLS");
+//		
+//	}
+//    
+// }
+//
+//for(int i=0 ; i<2 ; i++)
+//{
+//		total[i]=0;
+//		multiply[i]=1;
+//for(int j=0; j<2 ; j++)
+//{
+//
+//	total[i]=total[i]+number[j][i];
+//    multiply[i]=multiply[i]*number[i][j];
+//}
+//cout<<"the addition of the "<<i+1<<" column is : "<<total[i]<<endl;
+//cout<<"the multiplication of the "<<i+1<<" row is : "<<multiply[i]<<endl;
+//}
+
+
+ //cout<<****************************************************3"<<endl;
+ 
+// 
+// double mark[2][5];
+//    char exellent[10]="Exellent";
+//   char good[10]="good";
+//    char fair[10]="fair";
+//  char poor[10]="poor";
+//    char fail[10]="fail";
+//    double total_mark[2];
+//    
+//    for(int i=0 ; i<2 ; i++) {
+//        total_mark[i]=0;
+//        for(int j=0 ; j<5 ; j++) {
+//            cout<<"Enter marks for student "<<i+1<<" test, assignment, quiz, project, final respectively: ";
+//          cin>>mark[i][j];
+//          total_mark[i] += mark[i][j];
+//            system("CLS");
+//        }
+//    }
+//
+//    cout<<"Student"<<setw(10)<<"Test"<<setw(10)<<"Assignment"<<setw(10)<<"Quiz"<<setw(10)<<"Project"<<setw(10)<<"Final"<<setw(10)<<"Total"<<setw(10)<<"Status"<<endl;
+//    for(int i=0 ; i<2 ; i++) {
+//        cout<<"Student "<<i+1<<setw(10)<<mark[i][0]<<setw(10)<<mark[i][1]<<setw(10)<<mark[i][2]<<setw(10)<<mark[i][3]<<setw(10)<<mark[i][4]<<setw(10)<<total_mark[i]<<setw(10);
+//        if(total_mark[i]>80 ) {
+//            cout<<exellent<<endl;
+//      } else if(total_mark[i]>60 && total_mark[i]<80) {
+//            cout<<good<<endl;
+//      } else if(total_mark[i]>50 && total_mark[i]<60) {
+//            cout<<fair<<endl;
+//      } else if(total_mark[i]>40 && total_mark[i]<50) {
+//          cout<<poor<<endl;
+//       } else if(total_mark[i]<40) {
+//            cout<<fail<<endl;
+//      }
+//    }
+ 
+// 
+
+ //cout<<****************************************************4"<<endl;
+
+//
+// int arr[3][4]={},sum=0;
+//    for (int i = 0; i <3; i++)
+//    {
+//        for (int j = 0;j<4; j++)
+//        {
+//            cout<<"Enter Row "<<i+1<<" Column "<<j+1<<" element:  ";
+//            cin>>arr[i][j];
+//            system("cls");
+//            int temp= arr[i][j];
+//            if (temp%2==0)
+//            {
+//                sum+=temp;
+//            }
+//        }
+//    }
+//    cout<<"\nThe sum of even numbers in the array is "<<sum<<endl;
+//    for (int i = 0; i <3; i++)
+//    {
+//        for (int j = 0;j<4; j++)
+//        {
+//            int temp= arr[i][j];
+//            if (temp%2==0)
+//            {
+//                cout<<temp<<"\t";
+//            }
+//            else
+//            {
+//                cout<<"*"<<"\t";
+//            }
+//        }
+//        cout<<endl;
+//    }
+//    
+//    
+//         //cout<<****************************************************5"<<endl;
+//    
+//    
+//    int count=0;
+//    bool arr[5][5]={{0,1,0,1,1},
+//                    {1,0,1,0,1},
+//                    {0,1,0,0,0},
+//                    {1,0,0,0,1},
+//                    {1,1,0,1,0}};
+//    for (int i = 0; i <5; i++)
+//    {
+//        for (int j =i; j<5; j++)
+//        {
+//            bool x=arr[i][j],y=arr[j][i];
+//            if ((x==true)&&(y==true))
+//            {
+//                count++;
+//            }
+//            
+//        }
+//        
+//    }
+//    cout<<"The number of freindships(pair of friends) which are represented in this array is: "<<count;
+
+
+
+
+//cout<<****************************************************6"<<endl;
+
+
+//
+//char answer[11]={'A','B','C','D','C','A','B','A','D','C','\0'};
+//
+//char get_answer[10];
+//
+//char Answer;
+//
+//again:
+//for(int i=0 ; i<10 ; i++)
+//{
+//
+//	cout<<"enter your answer starting from question number [ "<<i<< " ] : ";
+//	cin>>Answer;
+//	get_answer[i]=toupper(Answer);
+//}
+//
+//int count =0;
+//for(int i=0 ; i<11 ; i++)
+//{
+//	if(get_answer[i]==answer[i])
+//	{
+//		count++;
+//	}
+//}
+// cout<<"correct answer "<<setw(15)<<"your answer"<<endl;
+//for(int i=0 ; i<11 ; i++)
+//{
+//	
+// cout<<answer[i]<<setw(15)<<get_answer[i]<<endl;
+//
+//}
+//
+//cout<<"your score out of 10 is : "<<count;
+
+
+
+//cout<<****************************************************7"<<endl;
+
+
+//
+//char first_name[20];
+//char middle_name[20];
+//char last_name[20];
+//
+//cout<<"enter first name : ";
+//cin>>first_name;
+//system("CLS");
+//cout<<"enter middle name : ";
+//cin>>middle_name;
+//system("CLS");
+//cout<<"enter last name : ";
+//cin>>last_name;
+//system("CLS");
+//
+//cout<<first_name[0]<<setw(5)<<middle_name[0]<<setw(5)<<last_name[0];
+
+
+//cout<<****************************************************7"<<endl;
+
+
+//char pass[5]={'1','2','3','4','5'};
+//char get_password[5];
+//tryt:
+//for(int i=0 ; i<5 ; i++)
+//{
+//    
+//	cout<<"enter the password";
+//	cin>>get_password[i];
+//
+//}
+//
+//
+//for(int i=0 ; i<6 ; i++)
+//{
+//	if(get_password[i]==pass[i])
+//	{
+//		continue;
+//	}
+//	else{
+//	    cout<<"the access is denied,please enter the correct password";
+//		goto tryt;	
+//	  }
+//}
+//
+//cout<<"access granted";
+
+
+//cout<<****************************************************7<<endl;
+
+
+// char favorate_tv[2][30];
+// 
+// strcpy(favorate_tv[0],"batman &superman");
+// 
+// strcpy(favorate_tv[1],"superman");
+//
+// for(int i =0 ; i<2 ;i++)
+// {
+// 	cout<<"my favorite film is : "<<favorate_tv[i]<<endl;
+// 	
+// }
+
+
+//cout<<****************************************************8<<endl;
+
+
+//string
+
+
+
+
+   //quesion number 1 reads numbers of vowels and consonant
+
+
+
+
+//int vowel = 0;
+//int consonant=0;
+//string letters;
+//cin>>letters;
+//
+//
+//for(int i=0;i<letters.length();i++)
+//{
+//	
+//		switch(toupper(letters[i]))
+//		{
+//			case 'A':
+//				count++;
+//				break;
+//			case 'U':
+//				count++;
+//					break;
+//			case 'O':
+//				count++;
+//					break;
+//			case 'E':
+//				count++;
+//					break;
+//			case 'I':
+//				count++;
+//					break;
+//			default:
+//				consonant=letters.length()-count;
+//					break;
+//		}	
+//	
+//}
+//
+//cout<<consonant<<setw(10)<<vowel;
+
+
+
+
+     //quesion number 2 palindrom
+
+
+     
+     
+//string name;
+//cin>>name;
+//int count = 0;
+//for(int i =name.length()-1 ; i>0 ; i-- )
+//{
+//	if(name[i]==name[0+count]){
+//		count++;
+//		if(count==name.length()-1)
+//		{
+//			cout<<"they are palindrom";
+//		}
+//	}
+//	else
+//	{
+//		cout<<"they are not palindrom";
+//		break;
+//	}
+//	
+//}
+
+
+    //question number 3 remove non alphabet characters
+//
+//string nonalphabet="yared";
+//cin>>nonalphabet;
+//
+//
+//for(int i=0;i<nonalphabet.length();i++){
+//	if(isalpha(nonalphabet[i])){
+//		continue;
+//	}
+//	else{
+//    nonalphabet[i]={};			
+//	}
+//}
+//
+//cout<<nonalphabet;
+
+
+
+
+//question number 4
+
+
+
+ //string input;
+//    cout << "Enter a line of text: ";
+//    getline(cin, input);
+//
+//    int numTokens = 0;
+//    for (int i = 0; i < input.length(); i++) {
+//        if (input[i] == ' ') {
+//            numTokens++;
+//        }
+//    }
+//    numTokens++; // Account for the last token
+//
+//    string tokens[numTokens];
+//    int index = 0;
+//    string currentToken = "";
+//    for (int i = 0; i < input.length(); i++) {
+//        if (input[i] != ' ') {
+//            currentToken += input[i];
+//        } else {
+//            tokens[index] = currentToken;
+//            currentToken = "";
+//            index++;
+//        }
+//    }
+//    tokens[index] = currentToken;
+//
+//    cout << "Tokens:" << endl;
+//    for (int i = 0; i < numTokens; i++) {
+//        cout << tokens[i] << endl;
+//    }
+
+
+
+
+//question number 5
+
+
+
+//
+// int count = 0;
+//  int consonant = 0;
+//  int digit[10] = {0};
+// int alpha[26] = {0};
+//  string frequency;
+// cin >> frequency;
+//
+//  for (int i = 0; i < frequency.length(); i++) {
+//    if (isalpha(frequency[i])) {
+//      char c = toupper(frequency[i]);
+//      if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+//        count++;
+//      } else {
+//        consonant++;
+//      }
+//      alpha[c - 'A']++;
+//    } else if (isdigit(frequency[i])) {
+//     digit[frequency[i] - '0']++;
+//    }
+//  }
+//
+//  cout << "Vowels: " << count << endl;
+//    cout << "Consonants: " << consonant << endl;
+//  cout << "Digits: ";
+//for (int i = 0; i < 10; i++) {
+//    if (digit[i] > 0) {
+//      cout << i << ": " << digit[i] << " "<< endl;;
+//    }
+//  }
+//  cout << endl;
+//  cout << "Alphabets: ";
+//  for (int i = 0; i < 26; i++) {
+//    if (alpha[i] > 0) {
+//      cout << char('A' + i) << ": " << alpha[i] << " "<< endl;;
+//    }
+//  }
+
+
+
+  
+  //question number 6
+
+
+
+  
+//  string input;
+//    cout << "Enter a line of text: ";
+//    getline(std::cin, input);
+//
+//    int charFreq[256] = {0}; 
+//
+//    for (int i = 0; i < input.length(); i++) {
+//        charFreq[input[i]]++; 
+//    }
+//
+//    char mostFrequentChar = '\0';
+//    char leastFrequentChar = '\0';
+//    int maxFreq = 0;
+//    int minFreq = input.length() + 1;
+//
+//    for (int i = 0; i < 256; i++) {
+//        if (charFreq[i] > 0) {
+//            if (charFreq[i] > maxFreq) {
+//                maxFreq = charFreq[i];
+//                mostFrequentChar = static_cast<char>(i);
+//            }
+//            if (charFreq[i] < minFreq) {
+//                minFreq = charFreq[i];
+//                leastFrequentChar = static_cast<char>(i);
+//            }
+//        }
+//    }
+//
+//    std::cout << "Character with largest frequency: " << mostFrequentChar << " (Frequency: " << maxFreq << ")" << std::endl;
+//    std::cout << "Character with smallest frequency: " << leastFrequentChar << " (Frequency: " << minFreq << ")" << std::endl;
+  
+
+
+//string 7th
+
+
+
+
+//  char name[5][20], temp[5];
+//  int i,j;
+//  for (i = 0; i < 5; i++)
+//  {
+//    cout<<"Enter the name of person"<<i+1<<": ";
+//    cin>>name[i];
+//  }
+//  for (i = 5; i >0; i--)
+//  {
+//    for (j = 0; j<=i; j++)
+//    {
+//        if (strcmp(name[j],name[j+1])>0)
+//        {
+//            strcpy(temp,name[j]);
+//            strcpy(name[j],name[j+1]);
+//            strcpy(name[j+1],temp);
+//        }
+//        
+//    }
+//    
+//  }
+
+//question number 8
+
+
+
+
+//string text;
+//  string upper, lower;
+//
+//  cout << "Enter a string: ";
+//  getline(cin, text);
+//
+//  while (text != "") {
+//    upper = "";
+//    lower = "";
+//    for (size_t i = 0; i < text.length(); i++) {
+//      upper += toupper(text[i]);
+//      lower += tolower(text[i]);
+//    }
+//  
+//    cout << "Uppercase: " << upper << endl;
+//    cout << "Lowercase: " << lower << endl;
+//
+//    cout << "\nEnter another string (or press Enter to exit): ";
+//    getline(cin, text);
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
