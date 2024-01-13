a7f30451f6253b5dc7a5a11628381c8608c4c645
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-01-13 12:26:46 +0300

    Update Q8.cpp

--- ETS1379_15_YARED_HABTAMU/Activity 4.2/String/Q8.cpp
+++ ETS1379_15_YARED_HABTAMU/Activity 4.2/String/Q8.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+int main()
+{
+  cout << "Enter your sentence "<<endl;
+  string sent;
+  char x, y;
+  getline(cin, sent);
+  char upper_sent[100] = { };
+  char lower_sent[100] = { };
 
+  for (int i = 0;i < sent.length();i++)
+  {
+    x = toupper(sent[i]);
+    y = tolower(sent[i]);
+    upper_sent[i] = x;
+    lower_sent[i] = y;
+  }
+  cout << "All in UPPERCASE : "<<upper_sent << endl;
+  cout << "All in LOWERCASE :"  << lower_sent;
+}
