d724384a1c2edf1115c093fcb26b3498d75841e0
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-01-13 12:38:47 +0300

    Update Q1.cpp

--- ETS1379_15_YARED_HABTAMU/Activity 4.2/2D/Q1.cpp
+++ ETS1379_15_YARED_HABTAMU/Activity 4.2/2D/Q1.cpp
@@ -1,1 +1,19 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+  int array1[] = { 10,29,39,40 };
+  int array2[] = { 10,29,39,4 };
+  int count = 0;
+  for (int i = 0;i < size(array1);i++)
+  {
+    if (array1[i] == array2[i]) {
+      count++;
+    }
+  }
+  if (count == size(array1))
+    cout << "They are identical";
+  else
+    cout << "They are not identical";
+  return 0;
+}
