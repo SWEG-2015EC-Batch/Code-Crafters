f6df40d8fd325954a6edec0fee6cb75ed15f85dd
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-01-13 12:27:37 +0300

    Update Q9.cpp

--- ETS1379_15_YARED_HABTAMU/Activity 4.2/String/Q9.cpp
+++ ETS1379_15_YARED_HABTAMU/Activity 4.2/String/Q9.cpp
@@ -1,1 +1,18 @@
 
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+  char favorite_tv[2][30];
+  strcpy(favorite_tv[0], "Sport On EBS");
+
+  cout << "Enter the second TV program :";
+  cin.getline(favorite_tv[1],30);
+
+  for (int i = 0;i < 2;i++)
+  {
+    cout << favorite_tv[i] << endl;
+  }
+  return 0;
+}
