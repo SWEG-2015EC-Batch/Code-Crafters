e7db9d020a14ad67cf50a29423d67f7138d69f59
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-01-13 12:28:33 +0300

    Update Q10.cpp

--- ETS1379_15_YARED_HABTAMU/Activity 4.2/String/Q10.cpp
+++ ETS1379_15_YARED_HABTAMU/Activity 4.2/String/Q10.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+int main()
+{
+  char pass[9]= "yared888";
+  char your_pass[9];
+  cout << "Enter the password :";
+  cin.getline(your_pass,9);
 
+  if (strcmp(pass, your_pass)==0)
+  {
+    cout << "Correcr passwrd \n";
+  }
+  else
+  {
+    cout << "Incorrect pass :";
+  }
+
+}
