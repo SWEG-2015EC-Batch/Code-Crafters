6ee0fe2e35fda28f9f5b704c6fa0568497865941
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-01-13 21:18:59 +0300

    Update Q5.cpp

--- ETS1379_15_YARED_HABTAMU/Activity 4.2/2D/Q5.cpp
+++ ETS1379_15_YARED_HABTAMU/Activity 4.2/2D/Q5.cpp
@@ -1,1 +1,27 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+  bool friends[5][5] = {
+    {false, true, true, false, false},
+    {true, false, true, true, false},
+    {true, true, false, false, true},
+    {false, true, false, false, true},
+    {false, false, true, true, false}
+  };
+
+  int count_pairs = 0;
+
+  for (int i = 0; i < 5; i++) {
+    for (int j = i + 1; j < 5; j++) {
+      if (friends[i][j]) {
+        count_pairs++;
+      }
+    }
+  }
+
+  cout << "There are *" << count_pairs << "* number of pairs of friends: " << endl;
+
+  return 0;
+}
