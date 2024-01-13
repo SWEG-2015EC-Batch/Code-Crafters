67dbaadad4665c50cdc29d2a55d3fb143eccae7e
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-01-13 21:42:09 +0300

    Update Q6.cpp

--- ETS1379_15_YARED_HABTAMU/Activity 4.2/1D/Q6.cpp
+++ ETS1379_15_YARED_HABTAMU/Activity 4.2/1D/Q6.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  const int maxSize = 100;
+  int total_mark[maxSize];
+  int count;
 
+  cout << "For how many students you want to calculate : ";
+  cin >> count;
+  for (int i = 0; i < count;i++) {
+    cout << "Enter total mark:";
+    cin >> total_mark[i];
+  }
+  for (int k = 0;k < count; k++) {
+    cout <<k+1<<": " << total_mark[k] << " ----- ";
+    if (total_mark[k] >= 80) {
+      cout << "Excellent";
+    }
+    else if (total_mark[k] >= 60) {
+      cout << "Very Good";
+    }
+    else if (total_mark[k] >= 50) {
+      cout << "Fair";
+    }
+    else if (total_mark[k] >= 40) {
+      cout <<"Poor";
+    }
+    else {
+      cout << "Fail";
+    }
+    cout << endl;
+
+  }
+  return 0;
+
+}
