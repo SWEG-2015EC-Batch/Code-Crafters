f0917ef641c7a4c67641099426dea86477aa8673
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-01-13 12:18:34 +0300

    Update Q2.cpp

--- ETS1379_15_YARED_HABTAMU/Activity 4.2/1D/Q2.cpp
+++ ETS1379_15_YARED_HABTAMU/Activity 4.2/1D/Q2.cpp
@@ -1,1 +1,17 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+  int temp[8] = {}, total = 0;
+  for (int i = 0; i < 8; i++) {
+    cout << "Enter number "<<i+1<<" :";
+    cin >> temp[i];
+    total += temp[i];
+  }
+  cout << "*************************** \n";
+  float average = total / 8;
+  for(int i=0;i<8;i++)
+    cout << temp[i]<<endl;
+  cout << "*************************** \n";
+  cout << "average= " << average << endl;
+}
