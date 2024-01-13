3567865fdb3538139e4064af26d8417c8d7dbe99
yared-Habtamu <149290215+yared-Habtamu@users.noreply.github.com>
2024-01-13 21:18:04 +0300

    Update Q4.cpp

--- ETS1379_15_YARED_HABTAMU/Activity 4.2/String/Q4.cpp
+++ ETS1379_15_YARED_HABTAMU/Activity 4.2/String/Q4.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+  cout << "Enter your text :";
+  string your_text;
+  getline(cin, your_text);
 
+  for (int i = 0;i < your_text.length();i++)
+  {
+    while (i < your_text.length() && your_text[i] != ' ')
+    {
+      cout << your_text[i];
+      i++;
+    }
+    cout <<endl;
+
+  }
+
+}
